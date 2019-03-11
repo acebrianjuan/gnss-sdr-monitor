ddc11bc53129678e34ff6d38f6249757cd155b39
Alejandro Exojo <alex@vikingsoftware.com>
2019-03-11 12:36:54 +0100

    Don't replot delegate's plots when not visible

--- src/cn0_delegate.cpp
+++ src/cn0_delegate.cpp
@@ -142,10 +142,13 @@ void Cn0_Delegate::paint(QPainter *painter, const QStyleOptionViewItem &option,
         plot = plots.at(index.row());
     }
 
-    plot->graph(0)->setData(x_data, y_data);
-    plot->graph(0)->rescaleAxes(true);
-    plot->xAxis->setRange(x_data.first(), x_data.last());
-    plot->replot();
+    if (plot->isVisible())
+    {
+        plot->graph(0)->setData(x_data, y_data);
+        plot->graph(0)->rescaleAxes(true);
+        plot->xAxis->setRange(x_data.first(), x_data.last());
+        plot->replot(QCustomPlot::rpQueuedReplot);
+    }
 
 
     double min_y = 0;
