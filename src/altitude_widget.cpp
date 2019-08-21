8cce4e37fd12efb808d2f655c1d7dd2f64e2bf51
Álvaro Cebrián Juan <acebrianjuan@gmail.com>
2019-08-21 13:52:38 +0200

    Add AltitudeWidget, a new custom widget to show the evolution of the altitude as reported by the receiver

---
+++ src/altitude_widget.cpp
@@ -0,0 +1,72 @@
+#include "altitude_widget.h"
+#include "ui_altitude_widget.h"
+
+#include "float.h"
+
+AltitudeWidget::AltitudeWidget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::AltitudeWidget)
+{
+    ui->setupUi(this);
+
+    QChart *chart = ui->graphicsView->chart();
+    QChartView *chartView = ui->graphicsView;
+
+    chart->setTitle("Altitude vs Time");
+    chart->legend()->hide();
+
+    chart->addSeries(&m_series);
+    chart->createDefaultAxes();
+    chart->axisX()->setTitleText("TOW [s]");
+    chart->axisY()->setTitleText("Altitude [m]");
+    chart->layout()->setContentsMargins(0, 0, 0, 0);
+    chart->setContentsMargins(-18, -18, -14, -16);
+
+    chartView->setRenderHint(QPainter::Antialiasing);
+    chartView->setContentsMargins(0, 0, 0, 0);
+
+    min_x = DBL_MAX;
+    min_y = DBL_MAX;
+
+    max_x = -DBL_MAX;
+    max_y = -DBL_MAX;
+}
+
+AltitudeWidget::~AltitudeWidget()
+{
+    delete ui;
+}
+
+void AltitudeWidget::enqueueNewData(qreal tow, qreal altitude)
+{
+    m_queue.enqueue(QPointF(tow, altitude));
+}
+
+void AltitudeWidget::redraw()
+{
+    if (!m_queue.isEmpty())
+    {
+        QChart *chart = ui->graphicsView->chart();
+        QChartView *chartView = ui->graphicsView;
+
+        QPointF p;
+
+        for (int i = 0; i < m_queue.size(); i++)
+        {
+            p = m_queue.at(i);
+
+            min_x = std::min(min_x, p.x());
+            min_y = std::min(min_y, p.y());
+
+            max_x = std::max(max_x, p.x());
+            max_y = std::max(max_y, p.y());
+        }
+
+        m_series.append(m_queue);
+
+        chart->axisX()->setRange(min_x, max_x);
+        chart->axisY()->setRange(min_y, max_y);
+
+        m_queue.clear();
+    }
+}
---
+++ src/altitude_widget.h
@@ -0,0 +1,36 @@
+#ifndef GNSS_SDR_MONITOR_ALTITUDE_WIDGET_H_
+#define GNSS_SDR_MONITOR_ALTITUDE_WIDGET_H_
+
+#include <QWidget>
+#include <QLineSeries>
+#include <QQueue>
+
+namespace Ui {
+class AltitudeWidget;
+}
+
+class AltitudeWidget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit AltitudeWidget(QWidget *parent = nullptr);
+    ~AltitudeWidget();
+
+public slots:
+    void enqueueNewData(qreal tow, qreal altitude);
+    void redraw();
+
+private:
+    Ui::AltitudeWidget *ui;
+    QtCharts::QLineSeries m_series;
+    QQueue<QPointF> m_queue;
+
+    double min_x;
+    double min_y;
+
+    double max_x;
+    double max_y;
+};
+
+#endif // GNSS_SDR_MONITOR_ALTITUDE_WIDGET_H_
